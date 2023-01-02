7e00ab6cbe12270167659c7af057fe7466b5ca45
victoryang00 <yangyiwei2000@gmail.com>
2023-01-01 23:02:54 -0800

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 extern crate core_affinity;
 
-use std::thread;
+use std::{
+    alloc::{alloc, dealloc, Layout},
+    thread, sync::{Arc, Mutex},
+};
+
+use core_affinity::CoreId;
 
 // shm get sizeof LLC slice and spawn process with CAT technology
 
 fn main() {
     // Retrieve the IDs of all active CPU cores.
     // let core_ids = core_affinity::get_core_ids().unwrap();
-    let core_ids = vec![CoreId::new(0),CoreId::new(16)];
+    let signal = Arc::new(Mutex::new(0));
+    let core_ids = core_affinity::get_core_ids().unwrap();
+
+    let core_ids = vec![core_ids[0],core_ids[16]];
     dbg!(core_ids.clone());
     // Create a thread for each active CPU core.
     let handles = core_ids
@@ -18,14 +26,21 @@ fn main() {
                 let res = core_affinity::set_for_current(id);
                 if res {
                     match id.id {
-                        0=>{ // P core affinity
+                        0 => {
+                            // P core affinity
+                            signal.lock();
+                            unsafe {
+                                let layout = Layout::new::<[u16; 100]>();
+                                let ptr = alloc(layout);
+
+                                *(ptr as *mut u16) = 42;
+                            }
+
 
                         }
-                        16=>{ // E core affinity
-
+                        16 => { // E core affinity
                         }
-                        _=>{}
-
+                        _ => {}
                     }
                 }
             })
