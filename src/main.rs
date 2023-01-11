38f26c550e81f801dcf680a917a823245c5120f6
victoryang00 <yangyiwei2000@gmail.com>
2023-01-11 13:26:00 -0800

    fix raw pointer

--- src/main.rs
+++ src/main.rs
@@ -10,60 +10,74 @@ use std::{
 };
 // shm get sizeof LLC slice and spawn process with CAT technology
 
+fn taskset(cpuid: usize) {
+    core_affinity::set_for_current(core_affinity::CoreId { id: cpuid });
+}
+
+fn p_core_thread(
+    running: Arc<AtomicBool>,
+    shared_array: *mut u8,
+    mut now: Arc<std::time::Instant>,
+) {
+    taskset(0);
+    let mut shared_array = unsafe { std::slice::from_raw_parts_mut(shared_array, 1024 as usize) };
+    for i in 0..1024 {
+        shared_array[i] = 1;
+    }
+    now = Arc::from(std::time::Instant::now());
+    running.store(true, Ordering::Release);
+    // wait for E core to finish
+    while running.load(Ordering::SeqCst) {
+        for i in 0..1024 {
+            shared_array[i] = 1;
+        }
+        break;
+    }
+    println!("E->P core latency: {:?}", now.elapsed());
+    running.store(false, Ordering::Release);
+}
+
+fn e_core_thread(
+    running: Arc<AtomicBool>,
+    shared_array: *mut u8,
+    mut now: Arc<std::time::Instant>,
+) {
+    taskset(16);
+    let mut shared_array = unsafe { std::slice::from_raw_parts_mut(shared_array, 1024 as usize) };
+    while running.load(Ordering::SeqCst) {
+        for i in 0..1024 {
+            shared_array[i] = 0;
+        }
+        break;
+    }
+    println!("P->E core latency: {:?}", now.elapsed());
+    now = Arc::from(std::time::Instant::now());
+
+    running.store(false, Ordering::Release);
+}
+
 fn main() {
     // Retrieve the IDs of all active CPU cores.
     // let core_ids = core_affinity::get_core_ids().unwrap();
-    let shared_array = Arc::new([0u8; 8 * 1024]);
+    let mut shared_array = [0u8; 8 * 1024];
+    // let mut handles = vec![];
+    let now = Arc::new(std::time::Instant::now());
+    for _ in 0..10 {
+        let running = Arc::new(AtomicBool::new(false));
 
-    let ptr = AtomicBool::new(false);
-    let core_ids = core_affinity::get_core_ids().unwrap();
-    let mut now = std::time::Instant::now();
+        let p_th = {
+            let running = running.clone();
+            let now = now.clone();
+            std::thread::spawn(move || p_core_thread(running, shared_array.as_mut_ptr(), now))
+        };
+        let e_th = {
+            let running = running.clone();
+            let now = now.clone();
 
-    let core_ids = vec![core_ids[0], core_ids[16]];
-    dbg!(core_ids.clone());
-    // Create a thread for each active CPU core.
-    let handles = core_ids
-        .into_iter()
-        .map(|id| {
-            thread::spawn(move || {
-                // Pin this thread to a single CPU core.
-                let res = core_affinity::set_for_current(id);
-                if res {
-                    match id.id {
-                        0 => {
-                            // P core affinity
-                            for i in 0..8 * 1024 {
-                                shared_array[i] = 1;
-                            }
-                            now = std::time::Instant::now();
-                            ptr.store(true, Ordering::Release);
-                            // wait for E core to finish
-                            while ptr.load(Ordering::SeqCst) {
-                                for i in 0..8 * 1024 {
-                                    shared_array[i] = 1;
-                                }
-                            }
-                            println!("E->P core latency: {:?}", now.elapsed());
-                            ptr.store(false, Ordering::Release);
-                        }
-                        16 => {
-                            // E core affinity
-                            while ptr.load(Ordering::SeqCst) {
-                                for i in 0..8 * 1024 {
-                                    shared_array[i] = 0;
-                                }
-                            }
-                            println!("P->E core latency: {:?}", now.elapsed());
-                            ptr.store(false, Ordering::Release);
-                        }
-                        _ => {}
-                    }
-                }
-            })
-        })
-        .collect::<Vec<_>>();
+            std::thread::spawn(move || e_core_thread(running, shared_array.as_mut_ptr(), now))
+        };
 
-    for handle in handles.into_iter() {
-        handle.join().unwrap();
+        // handles.push(p_th.join().unwrap());
+        // handles.push(e_th.join().unwrap());
     }
 }
