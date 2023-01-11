6cfc9d97ef0dafdb8263a7b2a846aaee73af0ee6
victoryang00 <yangyiwei2000@gmail.com>
2023-01-11 13:53:20 -0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,25 @@
 extern crate core_affinity;
+extern crate lazy_static;
 
-use crossbeam_epoch::{Atomic, Guard, Shared};
-use std::cell::RefCell;
+use lazy_static::lazy_static;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::{
-    alloc::{alloc, Layout},
-    sync::Arc,
-    thread,
-};
+use std::sync::Arc;
 // shm get sizeof LLC slice and spawn process with CAT technology
+
+lazy_static! {
+    static ref now: Arc<std::time::Instant> = Arc::new(std::time::Instant::now());
+}
 
 fn taskset(cpuid: usize) {
     core_affinity::set_for_current(core_affinity::CoreId { id: cpuid });
 }
 
-fn p_core_thread(
-    running: Arc<AtomicBool>,
-    shared_array: *mut u8,
-    mut now: Arc<std::time::Instant>,
-) {
+fn p_core_thread(running: Arc<AtomicBool>, shared_array: *mut u8) {
     taskset(0);
-    let mut shared_array = unsafe { std::slice::from_raw_parts_mut(shared_array, 1024 as usize) };
+    let shared_array = unsafe { std::slice::from_raw_parts_mut(shared_array, 1024 as usize) };
     for i in 0..1024 {
         shared_array[i] = 1;
     }
-    now = Arc::from(std::time::Instant::now());
     running.store(true, Ordering::Release);
     // wait for E core to finish
     while running.load(Ordering::SeqCst) {
@@ -37,13 +32,9 @@ fn p_core_thread(
     running.store(false, Ordering::Release);
 }
 
-fn e_core_thread(
-    running: Arc<AtomicBool>,
-    shared_array: *mut u8,
-    mut now: Arc<std::time::Instant>,
-) {
+fn e_core_thread(running: Arc<AtomicBool>, shared_array: *mut u8) {
     taskset(16);
-    let mut shared_array = unsafe { std::slice::from_raw_parts_mut(shared_array, 1024 as usize) };
+    let shared_array = unsafe { std::slice::from_raw_parts_mut(shared_array, 1024 as usize) };
     while running.load(Ordering::SeqCst) {
         for i in 0..1024 {
             shared_array[i] = 0;
@@ -51,7 +42,6 @@ fn e_core_thread(
         break;
     }
     println!("P->E core latency: {:?}", now.elapsed());
-    now = Arc::from(std::time::Instant::now());
 
     running.store(false, Ordering::Release);
 }
@@ -60,24 +50,21 @@ fn main() {
     // Retrieve the IDs of all active CPU cores.
     // let core_ids = core_affinity::get_core_ids().unwrap();
     let mut shared_array = [0u8; 8 * 1024];
-    // let mut handles = vec![];
-    let now = Arc::new(std::time::Instant::now());
+    let mut handles = vec![];
     for _ in 0..10 {
         let running = Arc::new(AtomicBool::new(false));
 
         let p_th = {
             let running = running.clone();
-            let now = now.clone();
-            std::thread::spawn(move || p_core_thread(running, shared_array.as_mut_ptr(), now))
+            std::thread::spawn(move || p_core_thread(running, shared_array.as_mut_ptr()))
         };
         let e_th = {
             let running = running.clone();
-            let now = now.clone();
 
-            std::thread::spawn(move || e_core_thread(running, shared_array.as_mut_ptr(), now))
+            std::thread::spawn(move || e_core_thread(running, shared_array.as_mut_ptr()))
         };
 
-        // handles.push(p_th.join().unwrap());
-        // handles.push(e_th.join().unwrap());
+        handles.push(p_th.join().unwrap());
+        handles.push(e_th.join().unwrap());
     }
 }
